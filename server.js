d154dfcb4bdfa60310d72c3678dc06ef975b6f2b
arthur barata <arthurpbarata@gmail.com>
2020-02-18 19:42:18 -0300

    added cors policy

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const routes = require('./app')
 dotenv.config()
 const App = Express()
 
+App.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+})
 App.use(routes);
 
 App.use(morgan('dev'))
